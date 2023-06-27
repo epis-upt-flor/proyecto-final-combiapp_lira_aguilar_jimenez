98fc5b372ef9ac24706b8c6831dc1f4065cc4752
Víctor Eleazar Aguilar Pinto <82843397+vctoraguilar@users.noreply.github.com>
2023-06-27 01:35:30 -0500

    MapActivity updated

--- app/src/main/java/com/rasgo/combidriver/activities/MapActivity.kt
+++ app/src/main/java/com/rasgo/combidriver/activities/MapActivity.kt
@@ -1,96 +1,185 @@
 package com.rasgo.combidriver.activities
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.content.res.Resources
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.drawable.Drawable
+import android.hardware.*
 import android.location.Location
-import android.os.Build
+import android.os.*
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+
 import android.util.Log
 import android.view.View
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.easywaylocation.EasyWayLocation
 import com.example.easywaylocation.Listener
 import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.SettingsClient
 import com.google.android.gms.location.Priority
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
+import com.google.firebase.firestore.ListenerRegistration
 import com.rasgo.combidriver.R
 import com.rasgo.combidriver.databinding.ActivityMapBinding
-import com.rasgo.combidriver.providers.AuthProvider
-import com.rasgo.combidriver.providers.GeoProvider
+import com.rasgo.combidriver.fragments.ModalBottomSheetBooking
+import com.rasgo.combidriver.fragments.ModalBottomSheetMenu
+import com.rasgo.combidriver.models.Booking
+import com.rasgo.combidriver.models.FCMBody
+import com.rasgo.combidriver.models.FCMResponse
+import com.rasgo.combidriver.providers.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class MapActivity : AppCompatActivity(), OnMapReadyCallback, Listener {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback, Listener, SensorEventListener {
 
-    private lateinit var binding:ActivityMapBinding
-    private var googleMap: GoogleMap?=null
-    private var easyWayLocation: EasyWayLocation? = null
-    private var myLocationLatLng: LatLng?=null
-    private var markerDriver:Marker?=null
-    private val geoProvider= GeoProvider()
-    private val authProvider= AuthProvider()
+    private var bearing: Float = 0.0f
+    private var bookingListener: ListenerRegistration? = null
+    private lateinit var binding: ActivityMapBinding
+    private var googleMap: GoogleMap? = null
+    var easyWayLocation: EasyWayLocation? = null
+    private var myLocationLatLng: LatLng? = null
+    private var markerDriver: Marker? = null
+    private val geoProvider = GeoProvider()
+    private val authProvider = AuthProvider()
+    private val bookingProvider = BookingProvider()
+    private val driverProvider = DriverProvider()
+    private val notificationProvider = NotificationProvider()
+    private val modalBooking = ModalBottomSheetBooking()
+    private val modalMenu = ModalBottomSheetMenu()
+
+    // SENSOR CAMERA
+    private var angle = 0
+    private val rotationMatrix = FloatArray(16)
+    private var sensorManager: SensorManager? = null
+    private var vectSensor: Sensor? = null
+    private var declination = 0.0f
+    private var isFirstTimeOnResume = false
+    private var isFirstLocation = false
+
+
+    val timer = object: CountDownTimer(30000, 1000) {
+        override fun onTick(counter: Long) {
+            Log.d("TIMER", "Counter: $counter")
+        }
+
+        override fun onFinish() {
+            Log.d("TIMER", "ON FINISH")
+            modalBooking.dismiss()
+        }
+
+    }
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
-        )
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
         val locationRequest = LocationRequest.create().apply {
             interval = 0
-            fastestInterval=0
+            fastestInterval = 0
             priority = Priority.PRIORITY_HIGH_ACCURACY
-            smallestDisplacement=1f
+            smallestDisplacement = 1f
         }
-        easyWayLocation = EasyWayLocation(this, locationRequest,false,false,this)
-        permissionLocation.launch(arrayOf(
+
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager?
+        vectSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
+
+        easyWayLocation = EasyWayLocation(this, locationRequest, false, false, this)
+
+        locationPermissions.launch(arrayOf(
             Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.ACCESS_COARSE_LOCATION
         ))
 
-        binding.btnConnect.setOnClickListener {connectDriver()}
-        binding.btnDisconnect.setOnClickListener {disconnectDriver()}
+        listenerBooking()
+        createToken()
 
+
+        binding.btnConnect.setOnClickListener { connectDriver() }
+        binding.btnDisconnect.setOnClickListener { disconnectDriver() }
+        binding.imageViewMenu.setOnClickListener { showModalMenu() }
     }
 
-    val permissionLocation=registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
-        permission->
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
-            when{
-              permission.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
-                  Log.d("LOCALIZACION", "Permiso concedido")
-                  //easyWayLocation?.startLocation()
-              }
+    val locationPermissions = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permission ->
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            when {
+                permission.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    Log.d("LOCALIZACION", "Permiso concedido")
+//                    easyWayLocation?.startLocation();
+                    checkIfDriverIsConnected()
+                }
                 permission.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
-                    Log.d("LOCALIZACION", "Permiso concedido con limitación")
-                    //easyWayLocation?.startLocation()
+                    Log.d("LOCALIZACION", "Permiso concedido con limitacion")
+//                    easyWayLocation?.startLocation();
                     checkIfDriverIsConnected()
                 }
                 else -> {
                     Log.d("LOCALIZACION", "Permiso no concedido")
                 }
+            }
+        }
 
+    }
+
+
+
+    private fun createToken() {
+        driverProvider.createToken(authProvider.getId())
+    }
+
+    private fun showModalMenu() {
+        modalMenu.show(supportFragmentManager, ModalBottomSheetMenu.TAG)
+    }
+
+    private fun showModalBooking(booking: Booking) {
+
+        val bundle = Bundle()
+        bundle.putString("booking", booking.toJson())
+        modalBooking.arguments = bundle
+        modalBooking.isCancelable = false // NO PUEDA OCULTAR EL MODAL BOTTTOM SHEET
+        modalBooking.show(supportFragmentManager, ModalBottomSheetBooking.TAG)
+        timer.start()
+    }
+
+    private fun listenerBooking() {
+        bookingListener = bookingProvider.getBooking().addSnapshotListener { snapshot, e ->
+            if (e != null) {
+                Log.d("FIRESTORE", "ERROR: ${e.message}")
+                return@addSnapshotListener
+            }
+
+            if (snapshot != null) {
+                if (snapshot.documents.size > 0) {
+                    val booking = snapshot.documents[0].toObject(Booking::class.java)
+                    if (booking?.status == "create") {
+                        showModalBooking(booking!!)
+                    }
+                }
             }
         }
     }
 
-    private fun checkIfDriverIsConnected(){
+
+
+    private fun checkIfDriverIsConnected() {
         geoProvider.getLocation(authProvider.getId()).addOnSuccessListener { document ->
             if (document.exists()) {
                 if (document.contains("l")) {
@@ -106,82 +195,61 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, Listener {
         }
     }
 
-    private fun saveLocation(){
-        if(myLocationLatLng!=null){
+    private fun saveLocation() {
+        if (myLocationLatLng != null) {
             geoProvider.saveLocation(authProvider.getId(), myLocationLatLng!!)
         }
     }
 
     private fun disconnectDriver() {
         easyWayLocation?.endUpdates()
-        if (myLocationLatLng!=null) {
+        if (myLocationLatLng != null) {
             geoProvider.removeLocation(authProvider.getId())
             showButtonConnect()
         }
     }
 
     private fun connectDriver() {
-        easyWayLocation?.endUpdates()
+        easyWayLocation?.endUpdates() // OTROS HILOS DE EJECUCION
         easyWayLocation?.startLocation()
         showButtonDisconnect()
     }
 
-    private fun showButtonConnect(){
-        binding.btnDisconnect.visibility = View.GONE
-        binding.btnConnect.visibility = View.VISIBLE
+    private fun showButtonConnect() {
+        binding.btnDisconnect.visibility = View.GONE // OCULTANDO EL BOTON DE DESCONECTARSE
+        binding.btnConnect.visibility = View.VISIBLE // MOSTRANDO EL BOTON DE CONECTARSE
     }
 
-    private fun showButtonDisconnect(){
-        binding.btnDisconnect.visibility = View.VISIBLE
-        binding.btnConnect.visibility = View.GONE
+    private fun showButtonDisconnect() {
+        binding.btnDisconnect.visibility = View.VISIBLE // MOSTRANDO EL BOTON DE DESCONECTARSE
+        binding.btnConnect.visibility = View.GONE // OCULATNDO EL BOTON DE CONECTARSE
     }
 
-    private fun addMarker(){
-        val drawable = ContextCompat.getDrawable(applicationContext, R.drawable.icon_combi_y)
-        val markerIcon=getMarkerfromDrawable(drawable!!)
-        if (markerDriver!=null){
-            markerDriver?.remove()
+    private fun addMarker() {
+        val drawable = ContextCompat.getDrawable(applicationContext, R.drawable.uber_car)
+        val markerIcon = getMarkerFromDrawable(drawable!!)
+        if (markerDriver != null) {
+            markerDriver?.remove() // NO REDIBUJAR EL ICONO
         }
-        if (myLocationLatLng!=null){
+        if (myLocationLatLng != null) {
             markerDriver = googleMap?.addMarker(
                 MarkerOptions()
                     .position(myLocationLatLng!!)
-                    .anchor(0.5f,0.5f)
+                    .anchor(0.5f, 0.5f)
                     .flat(true)
                     .icon(markerIcon)
             )
         }
-
     }
 
-    private fun getMarkerfromDrawable(drawable: Drawable): BitmapDescriptor {
-        val canvas = Canvas()
-        val bitmap = Bitmap.createBitmap(
-            100,
-            100,
-            Bitmap.Config.ARGB_8888
-        )
-        canvas.setBitmap(bitmap)
-        drawable.setBounds(0, 0, 100, 100)
-        drawable.draw(canvas)
-        return BitmapDescriptorFactory.fromBitmap(bitmap)
-    }
 
-    override fun onResume() {
-        super.onResume()
-
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        easyWayLocation?.endUpdates()
-    }
 
     override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+        googleMap?.uiSettings?.isZoomControlsEnabled = true
 
-        googleMap=map
-        googleMap?.uiSettings?.isZoomControlsEnabled=true
-        //easyWayLocation?.startLocation()
+//        easyWayLocation?.startLocation();
+        startSensor()
         if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
@@ -190,34 +258,56 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, Listener {
                 Manifest.permission.ACCESS_COARSE_LOCATION
             ) != PackageManager.PERMISSION_GRANTED
         ) {
+
             return
         }
-        googleMap?.isMyLocationEnabled=false
+        googleMap?.isMyLocationEnabled = false
 
         try {
             val success = googleMap?.setMapStyle(
-                MapStyleOptions.loadRawResourceStyle(this,R.raw.style)
+                MapStyleOptions.loadRawResourceStyle(this, R.raw.style)
             )
             if (!success!!) {
-                Log.d("MAPAS","No se encontró el estilo")
+                Log.d("MAPAS", "No se pudo encontrar el estilo")
             }
 
-        }catch (e:Resources.NotFoundException){
-            Log.d("MAPAS","Error ${e.toString()}")
+        } catch (e: Resources.NotFoundException) {
+            Log.d("MAPAS", "Error: ${e.toString()}")
         }
+
     }
 
     override fun locationOn() {
 
     }
 
-    override fun currentLocation(location: Location) {
-        myLocationLatLng=LatLng(location.latitude, location.longitude)
+    override fun currentLocation(location: Location) { // ACTUALIZACION DE LA POSICION EN TIEMPO REAL
+        myLocationLatLng = LatLng(location.latitude, location.longitude) // LAT Y LONG DE LA POSICION ACTUAL
+
+        val field = GeomagneticField(
+            location.latitude.toFloat(),
+            location.longitude.toFloat(),
+            location.altitude.toFloat(),
+            System.currentTimeMillis()
+        )
+
+        declination = field.declination
+
+//        if (!isFirstLocation) {
+//            googleMap?.moveCamera(CameraUpdateFactory.newCameraPosition(
+//                CameraPosition.builder().target(myLocationLatLng!!).zoom(19f).build()
+//            ))
+//            isFirstLocation = true
+//
+//        }
+//        val orientation = FloatArray(3)
+//        val bearing = Math.toDegrees(orientation[0].toDouble()).toFloat() + declination
+//        updateCamera(bearing)
 
         googleMap?.moveCamera(CameraUpdateFactory.newCameraPosition(
-            CameraPosition.builder().target(myLocationLatLng!!).zoom(17f).build()
+            CameraPosition.builder().target(myLocationLatLng!!).bearing(bearing).tilt(50f).zoom(19f).build()
         ))
-        addMarker()
+        addDirectionMarker(myLocationLatLng!!, angle)
         saveLocation()
     }
 
@@ -225,5 +315,93 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback, Listener {
 
     }
 
+    private fun updateCamera(bearing: Float) {
+        val oldPos = googleMap?.cameraPosition
+        val pos = CameraPosition.builder(oldPos!!).bearing(bearing).tilt(50f).build()
+        googleMap?.moveCamera(CameraUpdateFactory.newCameraPosition(pos))
+        if (myLocationLatLng != null) {
+            addDirectionMarker(myLocationLatLng!!, angle)
+        }
+    }
+
+    private fun addDirectionMarker(latLng: LatLng, angle: Int)  {
+        val circleDrawable = ContextCompat.getDrawable(applicationContext, R.drawable.ic_up_arrow_circle)
+        val markerIcon = getMarkerFromDrawable(circleDrawable!!)
+        if (markerDriver != null) {
+            markerDriver?.remove()
+        }
+        markerDriver = googleMap?.addMarker(
+            MarkerOptions()
+                .position(latLng)
+                .anchor(0.5f, 0.5f)
+                .rotation(angle.toFloat())
+                .flat(true)
+                .icon(markerIcon)
+        )
+    }
+
+    private fun getMarkerFromDrawable(drawable: Drawable): BitmapDescriptor {
+        val canvas = Canvas()
+        val bitmap = Bitmap.createBitmap(
+            120,
+            120,
+            Bitmap.Config.ARGB_8888
+        )
+        canvas.setBitmap(bitmap)
+        drawable.setBounds(0,0,120,120)
+        drawable.draw(canvas)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
+    }
+
+
+
+    override fun onDestroy() { // CIERRA APLICACION O PASAMOS A OTRA ACTIVITY
+        super.onDestroy()
+        easyWayLocation?.endUpdates()
+        bookingListener?.remove()
+        stopSensor()
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor.type == Sensor.TYPE_ROTATION_VECTOR) {
+            SensorManager.getRotationMatrixFromVector(rotationMatrix, event.values)
+            val orientation = FloatArray(3)
+            SensorManager.getOrientation(rotationMatrix, orientation)
+            if (Math.abs(Math.toDegrees(orientation[0].toDouble()) - angle) > 0.8 ) {
+                bearing = Math.toDegrees(orientation[0].toDouble()).toFloat() + declination
+                updateCamera(bearing)
+            }
+            angle = Math.toDegrees(orientation[0].toDouble()).toInt()
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+
+    }
+
+    private fun startSensor() {
+        if (sensorManager != null) {
+            sensorManager?.registerListener(this, vectSensor, SensorManager.SENSOR_STATUS_ACCURACY_LOW)
+        }
+    }
+
+    private fun stopSensor () {
+        sensorManager?.unregisterListener(this)
+    }
+
+    override fun onResume() {
+        super.onResume() // ABRIMOS LA PANTALLA ACTUAL
+        if (!isFirstTimeOnResume) {
+            isFirstTimeOnResume = true
+        }
+        else {
+            startSensor()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopSensor()
+    }
 
 }
