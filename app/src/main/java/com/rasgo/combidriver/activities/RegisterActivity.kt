7653f8094c93d3a5560c07c5b8750c05bbe674ca
Víctor Eleazar Aguilar Pinto <82843397+vctoraguilar@users.noreply.github.com>
2023-06-27 03:20:51 -0500

    Register Activity ok

--- app/src/main/java/com/rasgo/combidriver/activities/RegisterActivity.kt
+++ app/src/main/java/com/rasgo/combidriver/activities/RegisterActivity.kt
@@ -26,25 +26,23 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(binding.root)
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
 
-        binding.btnGoToLogin.setOnClickListener { goToLogin() }
+        binding.btnLogin.setOnClickListener { goToLogin() }
         binding.btnRegister.setOnClickListener { register() }
     }
 
     private fun register() {
         val name = binding.textFieldName.text.toString()
-        val lastname = binding.textFieldLastname.text.toString()
         val email = binding.textFieldEmail.text.toString()
         val phone = binding.textFieldPhone.text.toString()
         val password = binding.textFieldPassword.text.toString()
         val confirmPassword = binding.textFieldConfirmPassword.text.toString()
 
-        if (isValidForm(name, lastname, email, phone, password, confirmPassword)) {
+        if (isValidForm(name, email, phone, password, confirmPassword)) {
             authProvider.register(email, password).addOnCompleteListener {
                 if (it.isSuccessful) {
                     val driver = Driver(
                         id = authProvider.getId(),
                         name = name,
-                        lastname = lastname,
                         phone = phone,
                         email = email
                     )
@@ -76,7 +74,6 @@ class RegisterActivity : AppCompatActivity() {
 
     private fun isValidForm(
         name: String,
-        lastname: String,
         email: String,
         phone: String,
         password: String,
@@ -85,10 +82,6 @@ class RegisterActivity : AppCompatActivity() {
 
         if (name.isEmpty()) {
             Toast.makeText(this, "Debes ingresar tu nombre", Toast.LENGTH_SHORT).show()
-            return false
-        }
-        if (lastname.isEmpty()) {
-            Toast.makeText(this, "Debes ingresar tu apellido", Toast.LENGTH_SHORT).show()
             return false
         }
         if (email.isEmpty()) {
